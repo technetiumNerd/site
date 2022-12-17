77b4f7e690c0f9ad0bd22833c01f4489881172b5
Philip Olesen <nbphilip@gmail.com>
2022-12-17 07:04:42 +0100

    christians mor

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -13,11 +13,13 @@ function Sleep(ms: number) {
 
 <template>
   <h1 style="text-align: center;font-size: 48px;">Hej Christian</h1>
-  <div id="maindiv">
+  <div id="maindiv" style="display:block;">
     <h2 >Jeg har overvåget din mor i 14 dage og 19 sekunder, og jeg er bange for hendes tilstand.
     Hun har sagt skøre ting, og råbt af naboen.
     Naboen havde også været træls i en længere periode, hvilket der også er et kendt klip med.
     Ham snakker vi så ikke mere med.</h2>
+    <img src="../assets/christiansmor.jpg">
+    <p>Her er Christians mor.</p>
   </div>
 </template>
 
