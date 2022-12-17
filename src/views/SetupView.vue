261d3effd610facea7e99f06355829896b949c74
Philip Olesen <nbphilip@gmail.com>
2022-12-17 23:09:30 +0100

    public img noasset

--- src/views/SetupView.vue
+++ src/views/SetupView.vue
@@ -14,10 +14,10 @@ function Sleep(ms: number) {
 <template>
   <h1>BSPWM, SXHKD, Polybar, neovim, vscode</h1>
   <div id="maindiv">
-  <p><a href="/bspwm.png" target="_blank"><img src="/src/assets/bspwm.png"><span class="imgtext">Desktoppen på min bærbare.</span></a>
+  <p><a href="/bspwm.png" target="_blank"><img src="//bspwm.png"><span class="imgtext">Desktoppen på min bærbare.</span></a>
     Min desktop ser tilsvarende ud. Ingen bars på de ydre skærme.
     VScode med Vim-mode, selvfølgelig.</p>
-    <p><a href="/neofetch.png" target="_blank"><img src="/src/assets/neofetch.png"><span class="imgtext">Info om min laptop. Arch Linux.</span></a>
+    <p><a href="/neofetch.png" target="_blank"><img src="/neofetch.png"><span class="imgtext">Info om min laptop. Arch Linux.</span></a>
     Jeg er godt tilfreds med de 12 AMD-kerner.</p>
   </div>
 </template>
