84ef81e47f08cb9d05c41f134382aef7254eeee5
Philip Olesen <nbphilip@gmail.com>
2022-12-17 17:09:49 +0100

    philip

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,11 +12,9 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>En bog</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    <a href="https://www.goodreads.com/book/show/386162.The_Hitchhiker_s_Guide_to_the_Galaxy" target="_blank" rel="noopener">Hitchhiker's Guide to the Galaxy</a>
   </WelcomeItem>
 
   <WelcomeItem>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Surprise, det er Philip!</h1>
+    <p>Philip er en one-man-army, som det ikke er til at spøge med.
+    Han griner aldrig, og hører kun god musik, i modsætningen til de fleste.</p>
+    <p>Denne side består af en række vigtige overvejelser, som du ikke kan finde andre steder.
+    Det er fordi jeg ikke skriver dem andre steder, og fordi at jeg ikke skriver så ofte. 
+    Det er meget spændende.</p>
+    <img src="../assets/philip.jpg" alt="det er Philip">
+    <p>Det er Philip.</p>
   </div>
 </template>
 
 <style>
+img {
+  max-width: 100%;
+}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
